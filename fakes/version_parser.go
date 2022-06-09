144c4d7626eaed6cb3aec2e855ab06071c5fe293
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2022-06-09 12:36:55 -0700

    bunch of logging

--- fakes/version_parser.go
+++ fakes/version_parser.go
@@ -1,6 +1,7 @@
 package fakes
 
 import "sync"
+import "fmt"
 
 type VersionParser struct {
 	ParseVersionCall struct {
@@ -18,12 +19,19 @@ type VersionParser struct {
 }
 
 func (f *VersionParser) ParseVersion(param1 string) (string, error) {
+	fmt.Println("Parsing...")
 	f.ParseVersionCall.mutex.Lock()
+	fmt.Println("lock")
 	defer f.ParseVersionCall.mutex.Unlock()
+	fmt.Println("unlock")
 	f.ParseVersionCall.CallCount++
+	fmt.Println("inc callcount")
 	f.ParseVersionCall.Receives.Path = param1
+	fmt.Println("recieves.path set")
 	if f.ParseVersionCall.Stub != nil {
+		fmt.Println("returning stub")
 		return f.ParseVersionCall.Stub(param1)
 	}
+	fmt.Println("returning version")
 	return f.ParseVersionCall.Returns.Version, f.ParseVersionCall.Returns.Err
 }
