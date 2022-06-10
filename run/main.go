0e7a19698e5bd3d9cd73d757fb8511111b2c14ce
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2022-06-10 13:14:34 -0700

    remove v2, see if it works

--- run/main.go
+++ run/main.go
@@ -4,11 +4,11 @@ import (
 	"os"
 
 	dotnetcoreaspnet "github.com/Goldngamr5/dotnet-core-aspnet"
-	"github.com/paketo-buildpacks/packit/v2"
-	"github.com/paketo-buildpacks/packit/v2/cargo"
-	"github.com/paketo-buildpacks/packit/v2/chronos"
-	"github.com/paketo-buildpacks/packit/v2/draft"
-	"github.com/paketo-buildpacks/packit/v2/postal"
+	"github.com/paketo-buildpacks/packit"
+	"github.com/paketo-buildpacks/packit/cargo"
+	"github.com/paketo-buildpacks/packit/chronos"
+	"github.com/paketo-buildpacks/packit/draft"
+	"github.com/paketo-buildpacks/packit/postal"
 )
 
 func main() {
