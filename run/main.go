53363b126b6240483bb887ee7051264f91905b1e
Goldngamr5 <70986642+Goldngamr5@users.noreply.github.com>
2022-06-10 12:44:36 -0700

    change to link to this buildpack :D

--- run/main.go
+++ run/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	dotnetcoreaspnet "github.com/paketo-buildpacks/dotnet-core-aspnet"
+	dotnetcoreaspnet "github.com/Goldngamr5/dotnet-core-aspnet"
 	"github.com/paketo-buildpacks/packit/v2"
 	"github.com/paketo-buildpacks/packit/v2/cargo"
 	"github.com/paketo-buildpacks/packit/v2/chronos"
